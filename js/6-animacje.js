c7d31403165015583045ab5859dc9396f305a308
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-24 16:10:10 +0200

    animacje

--- js/6-animacje.js
+++ js/6-animacje.js
@@ -1,2 +1,23 @@
 'use strict';
 
+$(function(){
+   
+    //efekt hide i show
+    $('p').hide(3000).show(3000);
+    
+    //efekt fadeOut i fadeIn
+    $('h1').fadeOut(3000).fadeIn(3000);
+    
+    //efekt slideUp i slideDown
+    $('h2').slideUp(3000).slideDown(3000);
+    
+    //funkcja zwrotna animacji - tzw. callback
+    function animacja(){
+        console.log('zakończono pierwszą animację. Zaraz startuje druga :)');
+        
+        $('h3').animate({'font-size': '1em', 'margin-left': '0px'}, 2000)
+    }
+    
+    //efekt animacji z funckją zwrotną animacja
+     $('h3').animate({'font-size': '3em', 'margin-left': '100px'}, 2000, animacja);
+});
