65cc8cbd1ed7301069b787249dfeefedcbf2dfc1
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-24 14:30:52 +0200

    selektory

---
+++ js/2-selektory.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$(function(){
+    
+    //pobieranie elementów po znaczniku
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    //pobierani po id
+    var paragraphOne = $('#paragraf');
+    console.log(paragraphOne);
+    
+    //pobieranie po klasie
+    var paragraphTwo = $('.paragraf');
+    console.log(paragraphTwo);
+    
+    
+});
